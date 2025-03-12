acd49395bdee95da1304513cdb3f073433fa60df
pan171 <panzhong171@foxmail.com>
2025-03-12 17:43:23 +0800

    update

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ source activate pyqt
 
 pip install -r requirements.txt
 ```
+
+# Package
+
+```sh
+pyinstaller --noconfirm --onefile --windowed --paths pages --add-data "img;img" --add-data "data;data" --add-data "pages;pages" app.py
+```
--- pack.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-pyinstaller --noconfirm --onefile --windowed --paths src --add-data "img;img" --add-data "data;data" app.py
-
-pyinstaller --noconfirm --onefile --windowed --paths pages --add-data "img;img" --add-data "data;data" --add-data "pages;pages" app.py
